b880c3888dfe499ded61040e8979c2b9d741b1ba
Hanif adedotun <hanifadedotun2k19@gmail.com>
2021-09-10 12:47:01 +0100

    offline service worker

--- public/worker.js
+++ public/worker.js
@@ -1,7 +1,9 @@
 var CACHE_NAME = 'pwa-task-manager';
 var urlsToCache = [
-  '/'
+  '/',
+  '/offline.html'
 ];
+const OFFLINE_URL = '/offline.html';
 
 // Install a service worker
 self.addEventListener('install', event => {
@@ -22,13 +24,27 @@ self.addEventListener('fetch', event => {
       .then(function(response) {
         // Cache hit - return response
         if (response) {
+          console.log('ServiceWorker'+ response)
           return response;
         }
         return fetch(event.request);
       }
     )
+    .catch(function(error){
+      const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+    })
   );
 });
+
+// self.addEventListener('fetch', function(event){
+//   event.respondWith(
+//     fetch(event.request).catch(function(){
+//       return caches.match('/offline.html');
+//     })
+//   );
+// });
 
 // Update a service worker
 self.addEventListener('activate', event => {
