5e5bcfee2d48c75adbbfbbcee0169a3065ba7f9c
Hanif adedotun <hanifadedotun2k19@gmail.com>
2021-09-23 22:12:00 -0300

    Updates

--- worker.js
+++ worker.js
@@ -1,7 +1,9 @@
 var CACHE_NAME = 'pwa-task-manager';
 var urlsToCache = [
-  '/'
+  '/',
+  '/offline.html'
 ];
+const OFFLINE_URL = '/offline.html';
 
 // Install a service worker
 self.addEventListener('install', event => {
@@ -22,13 +24,22 @@ self.addEventListener('fetch', event => {
       .then(function(response) {
         // Cache hit - return response
         if (response) {
+          // console.log('ServiceWorker'+ JSON.stringify(response))
           return response;
+        }else if(event.request.headers.get('accept').includes('text/html')){
+          return caches.match('/offline.html');
         }
         return fetch(event.request);
       }
     )
+    // .catch(function(error){
+    //   const cache = await caches.open(CACHE_NAME);
+    //       const cachedResponse = await cache.match(OFFLINE_URL);
+    //       return cachedResponse;
+    // })
   );
 });
+
 
 // Update a service worker
 self.addEventListener('activate', event => {
