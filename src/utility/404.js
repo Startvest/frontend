f9d01ebac4a591fb11c79b0ace6d8e9efcb554f9
Hanif adedotun <hanifadedotun2k19@gmail.com>
2022-02-24 23:09:35 +0100

    404 page redesigned

--- src/utility/404.js
+++ src/utility/404.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import "./404.css";
+import {Back} from "react-bootstrap-icons";
+import {Button} from "react-bootstrap";
 
 function notFound(){
      return(
-          <div>
+          <div className='f04-cont'>
+               <div className='f04'>
                <h2>404</h2>
-               <p>Oops, seems this page does not exist on Startvest, go to our home page <a href='#/'>here</a></p>
+               <p>Oops, seems this page does not exist on Startvest, but you can check out our home page</p>
+               <Button className='hm-btn'><Back height="25" width="25"/><a href="/">Home</a></Button>
+               </div>
           </div>
      )
 }
